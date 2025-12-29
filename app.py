dee7de1717dd5288a80f16112bca40e14c468473
Pratik Jain <Pratikjain9109@gmail.com>
2025-12-29 18:01:24 +0530

    Initial Streamlit deployment for bone fracture detection

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from ultralytics import YOLO
+from PIL import Image
+
+st.set_page_config(page_title="Bone Fracture Detection")
+
+st.title("🦴 Bone Fracture Detection")
+st.write("Upload an X-ray image to detect fractures")
+
+@st.cache_resource
+def load_model():
+    return YOLO("best.pt")
+
+model = load_model()
+
+uploaded_file = st.file_uploader("Upload X-ray Image", type=["jpg", "png", "jpeg"])
+
+if uploaded_file:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    if st.button("Detect Fracture"):
+        with st.spinner("Detecting..."):
+            results = model(image)
+            result_image = results[0].plot()
+            st.image(result_image, caption="Detection Result", use_column_width=True)
